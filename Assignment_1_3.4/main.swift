06b1c69b2c2931911dfb1f43efd17574376ec9bd
MyLocalNum <satya@Satyas-MacBook-Air.local>
2018-01-25 14:09:44 -0600

    Initial Commit

---
+++ Assignment_1_3.4/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Assignment_1_3.4
+//
+//  Created by Satya Prakash on 1/25/18.
+//  Copyright © 2018 Satya Prakash. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
